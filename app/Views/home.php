ebad10a5ba3fa56325a0c777bbcabe5254c90d5a
nadyafadhila18 <64628797+nadyafadhila18@users.noreply.github.com>
2021-05-22 22:54:19 +0700

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,7 +1,63 @@
-<?= $this->extend('layout/template'); ?>
+<style>
+*{
+        
+        font-family: 'Reggae One', cursive;
+    }
+img{
+    width: 180px;
+    height: 180px;
+    align-content: center;
+}
+.container{
+    padding: 15%;
+    padding-left: 450px;
+    padding-top: 10px;
+}
+h1{
+    margin-top: 200px;
+    align
+}
+.btn{
+    background-color: #884141;
+    border: none;
+    color: #fff;
+    text-decoration: none;
 
-<?= $this->section('content'); ?>
+}
+.btn:hover {
+    background-color: #492227;
+    color: #fff;
+}
 
-<h1>Halaman home buat para users</h1>
+.btn:active {
+  background-color: #492227;
 
-<?= $this->endSection(); ?>
+}
+</style>
+
+<div class="container1">
+<h1 align="center">Welcome to World's Rank Game!</h1>
+
+    <div class="container">
+        <div class="row">
+          <div align="center" class="col-6 col-sm-4">Dinosaur Games</div>
+          <div align="center" class="col-6 col-sm-4">Flappy Bird</div>
+      
+          <!-- Force next columns to break to new line at md breakpoint and up -->
+          <div class="w-100 d-none d-md-block"></div>
+          <div align="center" class="col-6 col-sm-4"><img src="asset/dinosaur.png" alt="dinosaur">  </div>
+          <div align="center" class="col-6 col-sm-4"><img src="asset/Flappy_bird.png" alt="flappy"></div>
+          <div class="w-100 d-none d-md-block"></div>
+          <div align="center" class="col-6 col-sm-4"><button type="submit" class="btn btn">Leaderboard</button></div>
+          <div align="center" class="col-6 col-sm-4"><button type="submit" class="btn btn">Leaderboard</button></div>
+          <br> <br>
+          <div class="w-100 d-none d-md-block"></div>
+          <div align="center" class="col-6 col-sm-4"><button type="submit" class="btn btn">Record My Score</button></div>
+          <div align="center" class="col-6 col-sm-4"><button type="submit" class="btn btn">Record My Score</button></div>
+        </div>
+      </div>
+      
+
+
+
+</div>
